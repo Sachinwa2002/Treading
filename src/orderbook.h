eed41955122e96081f2bf817e15178e6fbb6f51b
Sachin Kumar <sachinkumar756492@gmail.com>
2026-06-15 11:55:24 +0530

    optimized with 4X faster

--- src/orderbook.h
+++ src/orderbook.h
@@ -5,18 +5,33 @@
 #include <deque>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 
 class OrderBook {
 
 private:
     static Order emptyOrder;
-    std::map<int64_t, std::deque<Order>, std::greater<int64_t>> bids;
-    std::map<int64_t, std::deque<Order>, std::less<int64_t>> asks;
+    // std::map<int64_t, std::deque<Order>, std::greater<int64_t>> bids;
+    // std::map<int64_t, std::deque<Order>, std::less<int64_t>> asks;
+    static const int64_t MAX_PRICE=450000;
+    static const int64_t MIN_PRICE=300000;
+    static const int64_t PRICE_LEVELS=MAX_PRICE-MIN_PRICE+1;
+    std::vector<std::deque<Order>>bids;
+    std::vector<std::deque<Order>>asks;
     std::unordered_map<uint64_t, OrderLocation> orderIndex;
     std::unordered_set<uint64_t> cancelledOrders;
 
+    int64_t bestBidIndex;
+    int64_t bestAskIndex;
+
 public:
-    void addOrder(Order order);
+    OrderBook():
+    bids(PRICE_LEVELS),
+    asks(PRICE_LEVELS),
+    bestBidIndex(MIN_PRICE),
+    bestAskIndex(MAX_PRICE){}
+
+    void addOrder(Order &order);
     void cancelOrder(uint64_t orderId);
     Order& getBestBid();
     Order& getBestAsk();
